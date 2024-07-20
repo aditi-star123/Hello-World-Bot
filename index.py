58f6a6024201849dda24452d484c66010f9b16eb
Aditi Tomar <112871901+aditi-star123@users.noreply.github.com>
2024-07-20 12:38:36 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,4 @@
+import telegram.ext
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+TOKEN = "7319650527:AAEa0UA3HFaRAfACXeiadSs-JDVrG7xwO3Q" #token generated using Telegram app
